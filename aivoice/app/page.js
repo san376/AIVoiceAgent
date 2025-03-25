158cd055233a18eb8002793928d47c06a4f81909
san376 <155067913+san376@users.noreply.github.com>
2025-03-25 10:57:59 +0530

    create

--- aivoice/app/page.js
+++ aivoice/app/page.js
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@stackframe/stack";
 import Image from "next/image";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div>
       <h2>Hello</h2>
       <Button variant={'destructive'}>Subscribe</Button>
+      <UserButton/>
     </div>
   );
 }
